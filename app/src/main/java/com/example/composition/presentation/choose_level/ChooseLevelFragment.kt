59c0e6f7e9bebce6d0376ffbda5f0ba34bb37a6b
Vitalii2401 <movsesyan.vitaliy24ua@gmail.com>
2023-01-29 20:02:00 +0200

    Launch GameFragment.kt when user selected level

--- app/src/main/java/com/example/composition/presentation/choose_level/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/composition/presentation/choose_level/ChooseLevelFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.composition.R
 import com.example.composition.databinding.FragmentChooseLevelBinding
+import com.example.composition.domain.entities.Level
+import com.example.composition.presentation.game.GameFragment
 
 class ChooseLevelFragment : Fragment() {
 
@@ -18,6 +21,35 @@ class ChooseLevelFragment : Fragment() {
     ): View {
         binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            buttonLevelTest.setOnClickListener {
+                launchGameFragment(Level.TEST)
+            }
+
+            buttonLevelEasy.setOnClickListener {
+                launchGameFragment(Level.EASY)
+            }
+
+            buttonLevelNormal.setOnClickListener {
+                launchGameFragment(Level.NORMAL)
+            }
+
+            buttonLevelHard.setOnClickListener {
+                launchGameFragment(Level.HARD)
+            }
+        }
+    }
+
+    private fun launchGameFragment(level: Level) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view, GameFragment.newInstance(level))
+            .addToBackStack(null)
+            .commit()
     }
 
     companion object {
--- app/src/main/java/com/example/composition/presentation/game/GameFragment.kt
+++ app/src/main/java/com/example/composition/presentation/game/GameFragment.kt
@@ -6,10 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.composition.databinding.FragmentGameBinding
+import com.example.composition.domain.entities.Level
 
 class GameFragment : Fragment() {
 
     private lateinit var binding: FragmentGameBinding
+    private lateinit var level: Level
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +25,22 @@ class GameFragment : Fragment() {
     ): View {
         binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun parseArgs() {
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+
+    companion object {
+
+        private const val KEY_LEVEL = "level"
+
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 }
