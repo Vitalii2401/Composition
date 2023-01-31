ad8fcc729c81a3073b736827fc15af0b82524f26
Vitalii2401 <movsesyan.vitaliy24ua@gmail.com>
2023-01-31 13:05:11 +0200

    Added data binding to fragment_game.xml

--- app/src/main/java/com/example/composition/presentation/BindingAdapter.kt
+++ app/src/main/java/com/example/composition/presentation/BindingAdapter.kt
@@ -1,10 +1,18 @@
 package com.example.composition.presentation
 
+import android.content.res.ColorStateList
+import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.example.composition.R
 import com.example.composition.domain.entities.GameResult
+
+interface OnOptionClickListener {
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("gameFinishedTVRequiredAnswers")
 fun requiredAnswers(textView: TextView, count: Int) {
@@ -49,3 +57,38 @@ fun smileResource(imageView: ImageView, winner: Boolean) {
 
 private fun getSmileResId(winner: Boolean): Int =
     if (winner) R.drawable.ic_smile_good else R.drawable.ic_smile_sad
+
+@BindingAdapter("gameFragmentPBPercentOfRightAnswers")
+fun bindMinProgress(progressBar: ProgressBar, value: Int) {
+    progressBar.setProgress(value, true)
+}
+
+@BindingAdapter("gameFragmentPBTint")
+fun bindProgressTint(progressBar: ProgressBar, state: Boolean) {
+    val color = getColorByState(state, progressBar)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+@BindingAdapter("gameFragmentTVTextColor")
+fun bindProgressTint(textView: TextView, state: Boolean) {
+    val color = getColorByState(state, textView)
+    textView.setTextColor(color)
+}
+
+private fun getColorByState(state: Boolean, view: View): Int {
+    val colorResId =
+        if (state) android.R.color.holo_green_light else android.R.color.holo_red_light
+    return ContextCompat.getColor(view.context, colorResId)
+}
+
+@BindingAdapter("gameFragmentTVNumberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("gameFragmentTVOnOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
+    }
+}
